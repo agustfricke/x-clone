b12eb5507bc8b39fc42b4e2921639250a9a87cfe
Agust Fricke <agustfricke@protonmail.com>
2023-09-30 22:13:26 -0300

    add basic struct

---
+++ handlers/auth.go
@@ -0,0 +1,15 @@
+package handlers
+
+import "github.com/gofiber/fiber/v2"
+
+func SignIn(c *fiber.Ctx) error {
+}
+
+func SignUp(c *fiber.Ctx) error {
+
+}
+
+func Logout(c *fiber.Ctx) error {
+
+}
+
